2f188edfdc730401f2ae59b37b3c8873b6927171
fjiz <crazy_crawler@gmx.net>
2016-05-18 23:02:19 +0200

    Alles eingerichtet, ein paar Sachen ausprobiert >> leider nix bei rausgekommen.

--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -1,9 +1,50 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GameFrame extends JFrame
-{
+public class GameFrame extends JFrame {
+
+	// Classmember
 	JTextField playType = new JTextField();
+	Dimension mainFrame = new Dimension(200, 500);
+	JButton btn = new JButton("main button");
+	JPanel backPane = new JPanel();
+
+	public GameFrame() {
+		super("WordPong");
+		
+		add(backPane, BorderLayout.CENTER);
+		backPane.setLayout(new GridLayout(1,3));
+		backPane.add(btn);
+
+		// Adding Menu
+		JMenuBar menu = new JMenuBar();
+		setJMenuBar(menu);
+		JMenu about = new JMenu("About");
+		menu.add(about);
+		JMenuItem rules = new JMenuItem("Rules");
+		about.add(rules);
+
+		// Make it visible
+		setResizable(false);
+		setSize(mainFrame);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 }
---
+++ src/gui/TestGui.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class TestGui {
+
+	public static void main(String[] args) {
+		GameFrame test = new GameFrame();
+	}
+
+}
