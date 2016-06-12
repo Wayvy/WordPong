1c7ac0e431a2fd9cb0417e1e9df30bfea1779387
Wavy <w.nabok@gmail.com>
2016-06-12 19:38:22 +0200

    Prepare merge

--- src/infrastructure/ConnectionController.java
+++ src/infrastructure/ConnectionController.java
@@ -19,53 +19,52 @@ public class ConnectionController extends Thread {
 	private String address;
 
 	/**
-	 * The Connection controller handles the hosting and the joining of one player to another.
-	 * The methods hostGame and joinGame create new Runnable's, that will run in the background 
-	 * and the check the connection to the other player.
-	 * Also the start of the game should be initiated in here.
+	 * The Connection controller handles the hosting and the joining of one
+	 * player to another. The methods hostGame and joinGame create new
+	 * Runnable's, that will run in the background and the check the connection
+	 * to the other player. Also the start of the game should be initiated in
+	 * here.
 	 */
 	public ConnectionController(int port) {
 		this.port = port;
 	}
-	
+
 	public ConnectionController(int port, String address) {
 		this.address = address;
 		this.port = port;
 	}
 
 	/**
-	 * Host the game and lets other people connect to this Socket.
-	 * Runs in the background, and checks the connection and creates the 
-	 * GameThread, once the connection to the other player has been accepted.
+	 * Host the game and lets other people connect to this Socket. Runs in the
+	 * background, and checks the connection and creates the GameThread, once
+	 * the connection to the other player has been accepted.
 	 */
 	public void hostGame() {
 		System.out.println("It hosts");
 		Thread hostThread = new Thread() {
 
-//			@Override
+			// @Override
 			public void run() {
 				try {
 					System.out.print("Host Game");
 					ServerSocket host = new ServerSocket(port, 100);
 					nemesis = host.accept();
-					
-					//Setup Sending Messages + Send a Message
+
+					// Setup Sending Messages + Send a Message
 					putputStream = nemesis.getOutputStream();
 					writer = new PrintWriter(putputStream);
 					writer.println("Hello i Am Host");
 					writer.flush();
-					
-					//Setup receiving messages + get a message
+
+					// Setup receiving messages + get a message
 					inputStream = nemesis.getInputStream();
 					reader = new Scanner(inputStream);
 					String message = reader.nextLine();
-					System.out.println("Received: "+  message);
+					System.out.println("Received: " + message);
 				} catch (IOException e) {
 					// TODO Maybe the client has to resends the dataStreams
 					e.printStackTrace();
-				}
-				finally 
-				{
+				} finally {
 					reader.close();
 					writer.close();
 					try {
@@ -76,57 +75,46 @@ public class ConnectionController extends Thread {
 					}
 				}
 			};
-			
 
 		};
-		
+
 		hostThread.start();
-		
 
 	}
 
 	/**
-	 * Joining an already existing host 
+	 * Joining an already existing host
 	 */
 	public void joinGame() {
 		Thread joinThread = new Thread() {
 
-//			@Override
+			// @Override
 			public void run() {
 
 				try {
 					System.out.println(port);
 					nemesis = new Socket(InetAddress.getByName(address), port);
 					System.out.println(nemesis);
-					
-					//Setup receiving Messages
+
+					// Setup receiving Messages
 					inputStream = nemesis.getInputStream();
 					reader = new Scanner(inputStream);
-					
-					//Setup sending Messages
+
+					// Setup sending Messages
 					putputStream = nemesis.getOutputStream();
 					writer = new PrintWriter(putputStream);
-					
+
 					String message = reader.nextLine();
 					System.out.println("RECEIVED MESSAGE: " + message);
-					
-					//String message = reader.nextLine();
-					System.out.println("Message: " + message);
 
-					
-					
 					writer.write("Hello I am Client");
 					writer.flush();
-					
-					
-					
+
 				} catch (IOException e) {
 					// TODO Something with the IOStreams is wrong, how can that
 					// be solved??
 					e.printStackTrace();
-				}
-				finally
-				{
+				} finally {
 					reader.close();
 					writer.close();
 					try {
@@ -135,12 +123,12 @@ public class ConnectionController extends Thread {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
+
 				}
 
 			}
 		};
-		
+
 		joinThread.start();
 	}
 }
